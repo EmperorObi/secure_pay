34a76039500a53575e540fc59b41208b39a1c166
Emperor OBI <osowo.obi11@gmail.com>
2024-10-03 14:59:21 +0100

    Initial commit

---
+++ script/landingPage.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//         const observer = new IntersectionObserver(function(entries) {
+//             entries.forEach(entry => {
+//                 if (entry.isIntersecting) {
+//                     entry.target.classList.add('show');
+//                 }
+//             });
+//         }, { threshold: 0.1 }); // Adjust threshold to trigger earlier or later
+    
+//         const iphoneSection = document.querySelector('.iphone-container');
+//         observer.observe(iphoneSection);
+//     });
