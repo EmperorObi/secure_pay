8dc6e696d7bd3c1b158ff75da1b41caa03cec0e5
Emperor OBI <osowo.obi11@gmail.com>
2024-11-05 14:23:28 +0100

    updated

--- script/landingPage.js
+++ script/landingPage.js
@@ -1,12 +1,18 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//         const observer = new IntersectionObserver(function(entries) {
-//             entries.forEach(entry => {
-//                 if (entry.isIntersecting) {
-//                     entry.target.classList.add('show');
-//                 }
-//             });
-//         }, { threshold: 0.1 }); // Adjust threshold to trigger earlier or later
-    
-//         const iphoneSection = document.querySelector('.iphone-container');
-//         observer.observe(iphoneSection);
-//     });
+const hamMenu = document.querySelector(".nav-toggle");
+
+const offScreenMenu = document.querySelector(".off-screen-navBar");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+// on scroll add box-shadow
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navBar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-shadow');
+  } else {
+    navbar.classList.remove('navbar-shadow');
+  }
+});
